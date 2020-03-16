af8a5a6a6a5c9194c71ac73118ae05bd777f8d72
kramarajan <ram.sjc05@gmail.com>
2020-03-16 08:05:34 +0530

    Specflow

---
+++ AutoFrameworkMobile/AutoFramework/Pages/PageElements/MobileMenu.cs
@@ -0,0 +1,35 @@
+﻿namespace AutoFramework.Pages
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
+
+    public class MobileMenu
+    {
+        public MobileMenu()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-post_type.menu-item-object-page.current-menu-item.page_item.page-item-17.current_page_item.menu-item-25")]
+        public IWebElement Introduction { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-taxonomy.menu-item-object-category.menu-item-106")]
+        public IWebElement Selectors { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-taxonomy.menu-item-object-category.menu-item-35")]
+        public IWebElement SpecialElements { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-taxonomy.menu-item-object-category.menu-item-57")]
+        public IWebElement TestCases { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-taxonomy.menu-item-object-category.menu-item-58")]
+        public IWebElement TestScenarios { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > nav > div > ul > li.menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-26")]
+        public IWebElement About { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#mh-mobile > div > div.mh-header-mobile-nav.clearfix > div > a")]
+        public IWebElement OpenMenu { get; set; }
+
+    }
+}
---
+++ SeleniumSpecflow/SeleniumSpecflow/StepBindings/SampleSteps.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumSpecflow.StepBindings
+{
+    [Binding]
+    public class SampleSteps
+    {
+
+        IWebElement element = null;
+       
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            //Driver
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://testing.todvachev.com/");
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+           
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            //Thread.Sleep(3000);
+            System.Console.ReadKey();
+            //driver.Quit();
+        }
+    }
+}
